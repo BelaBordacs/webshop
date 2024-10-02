c7a06a8ba4ba36d830628f51fc633277af7bcd6e
BelaBordacs <side1111@hotmail.com>
2024-10-02 18:30:28 +0200

    uj js fajlok, lista feltoltes

---
+++ modell/TermekekModell.js
@@ -0,0 +1,11 @@
+import { LIST } from "./productList.js"
+
+export default  class TermekekModell {
+    constructor (){
+        this.productList = [LIST]
+    };
+
+    getTermekek() {
+        return this.termekLista;
+    }
+};
---
+++ view/PublikusTermekek.js
@@ -0,0 +1,5 @@
+export default  class PublikusTermekek {
+    controller (){
+        
+    };
+};
