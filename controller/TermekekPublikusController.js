1eeb8d7defe9f82c97e628d840224a02770756dd
BelaBordacs <side1111@hotmail.com>
2024-10-03 20:55:14 +0200

    kosár modell, controller, és stílus

--- controller/TermekekPublikusController.js
+++ controller/TermekekPublikusController.js
@@ -7,12 +7,22 @@ export default class TermekekPublikusController {
   constructor() {
     // Modellek példányosítása
     this.termekekModell = new TermekekModell();
+    this.kosarModell = new KosarModell();
 
     // View-k példányosítása
     this.termekView = new PublikusTermekek('#termekek-container', this.hozzaadKosarhoz.bind(this));
+    this.kosarView = new KosarTermekek('#kosar-container');
 
     // Termékek megjelenítése
     this.termekView.megjelenitTermekek(this.termekekModell.getTermekek());
+
+    // Kosár frissítése kezdetben (üres)
+    this.kosarView.frissitKosar(this.kosarModell.getKosarTartalma());
   }
 
+  // Callback függvény a kosárba gombhoz
+  hozzaadKosarhoz(termek) {
+    this.kosarModell.hozzaadKosarhoz(termek);
+    this.kosarView.frissitKosar(this.kosarModell.getKosarTartalma());
+  }
 }
